3991c36c1b4fad4bc914d5fbfd64e9798d752507
Isha Aggarwal <isha0570@gmail.com>
2022-09-03 20:23:52 +0530

    First Commit

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,8 +1,44 @@
-import React from "react";
-const Counter = () => {
+import {useState} from "react";
+
+const Counter = (props) => {
+  console.log(props);
+  let [value, setValue] = useState(0);
+  const [disable, setDisable] = useState(true);
+  const onAddition = () => {
+    value = value + 1;
+    setValue(value);
+    console.log({value});
+    if (value > 0) {
+      setDisable(false);
+    }
+  };
+  const onSubtraction = () => {
+    value = value - 1;
+    setValue(value);
+    if (value === 0) {
+      setDisable(true);
+    }
+  };
+  const onOdd = () =>{
+    if(value%2!=0){
+    console.log({value},"is odd");
+    }
+  }
+  const onEven = () =>{
+    if(value%2===0){
+      console.log({value},"is even")
+    }
+  }
+
   return (
     <>
-      <div>counter</div>
+      <button onClick={onAddition} disabled={disable}>
+        +
+      </button>
+      <h1> {value}</h1>
+      <button onClick={onSubtraction} disabled={disable}>
+        -
+      </button>
     </>
   );
 };
